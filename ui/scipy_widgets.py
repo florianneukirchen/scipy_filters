d09ac418c6945fd758fdab6e0efb60ae7fb57d5b
Florian Neukirchen <mail@riannek.de>
2024-03-14 07:23:18 +0100

    start a helpers module

--- ui/scipy_widgets.py
+++ ui/scipy_widgets.py
@@ -1,8 +1,7 @@
 # https://github.com/qgis/QGIS/blob/master/python/plugins/processing/algs/qgis/ui/HeatmapWidgets.py
 
 import os
-import json
-import numpy as np
+
 from processing.gui.wrappers import WidgetWrapper
 from processing.tools import dataobjects
 from qgis.PyQt import uic
@@ -137,29 +136,3 @@ class OddSizesWidgetWrapper(SizesWidgetWrapper):
 
 
 
-
-def str_to_int_or_list(s):
-    """
-    Allow to have parameters for axes (one or several) or size (for all or each dimension)
-    """
-    out = None
-    try:
-        out = int(s)
-    except ValueError:
-        pass
-    if out:
-        return out
-    
-    if not (s[0] == "[" and s[-1] == "]"):
-        s = "[" + s + "]"
-
-    try:
-        decoded = json.loads(s)
-        a = np.array(decoded, dtype=np.int32)
-    except (json.decoder.JSONDecodeError, ValueError, TypeError):
-        raise ValueError('Can not parse string to array!')
-    
-    if a.ndim != 1:
-        raise ValueError('Wrong dimensions!')
-    
-    return a.tolist()
