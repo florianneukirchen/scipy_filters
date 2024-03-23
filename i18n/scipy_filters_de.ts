de98caecc5841803f86b4c74a4f664498471c6d1
Florian Neukirchen <mail@riannek.de>
2024-03-23 20:59:25 +0100

    Move Dimensions class out of the baseclass, it confuses pylupdate5

--- i18n/scipy_filters_de.ts
+++ i18n/scipy_filters_de.ts
@@ -1,37 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de_DE">
+<TS version="2.1">
 <context>
     <name>Dimensions</name>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="174"/>
         <source>Input layer</source>
-        <translation>Eingabe-Layer</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="183"/>
         <source>Dimension</source>
-        <translation>Dimensionen</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="203"/>
         <source>Output data type</source>
-        <translation>Ausgabe-Datentyp</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="289"/>
         <source>Failed to open Mask Layer</source>
-        <translation>Masken-Layer konnte nicht geöffnet werden</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="414"/>
         <source>3D only possible if input layer has more than 1 bands</source>
-        <translation>3D ist nur bei Layer mit mehr als 1 Band möglich</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="438"/>
         <source>Warning, the range of output datatype is not in the range of the input datatype. Clipping is likely.</source>
-        <translation>Warnung, das Werteintervall des Ausgabe-Datentyps ist nicht innerhalb desjenigen des Eingabe-Datentyp. Vermutlich werden Werte der Ausgabe beschnitten.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -39,52 +39,52 @@
     <message>
         <location filename="../ui/StructureWidget.ui" line="14"/>
         <source>Form</source>
-        <translation>Form</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/SizesWidget.ui" line="20"/>
         <source>Size (all axes)</source>
-        <translation>Größe (alle Achsen)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/SizesWidget.ui" line="37"/>
         <source>Size (bands)</source>
-        <translation>Größe (Bänder)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/SizesWidget.ui" line="47"/>
         <source>Size (rows)</source>
-        <translation>Größe (Zeilen)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/SizesWidget.ui" line="57"/>
         <source>Size (cols)</source>
-        <translation>Größe (Spalten)</translation>
-    </message>
-    <message>
-        <location filename="../ui/StructureWidget.ui" line="29"/>
-        <source>Load</source>
-        <translation>Laden</translation>
-    </message>
-    <message>
-        <location filename="../ui/StructureWidget.ui" line="67"/>
-        <source>TextLabel</source>
-        <translation>#TextLabel</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/OriginWidget.ui" line="23"/>
         <source>Cols</source>
-        <translation>Spalten</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/OriginWidget.ui" line="46"/>
         <source>Rows</source>
-        <translation>Zeilen</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/OriginWidget.ui" line="53"/>
         <source>Bands</source>
-        <translation>Bänder</translation>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/StructureWidget.ui" line="29"/>
+        <source>Load</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/StructureWidget.ui" line="67"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -92,12 +92,12 @@
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="518"/>
         <source>Border Mode</source>
-        <translation>Randmodus</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="524"/>
         <source>Constant value past edges for border mode &quot;constant&quot;</source>
-        <translation>Konstanter Wert jenseits des Rands für Modus &quot;Konstante&quot;</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -105,12 +105,12 @@
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="557"/>
         <source>Axis</source>
-        <translation>Achse</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="587"/>
         <source>Band axis not possible in 2D case</source>
-        <translation>Band-Achse gibt es in 2D nicht</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -118,7 +118,7 @@
     <message>
         <location filename="../algs/scipy_statistical_algorithms.py" line="305"/>
         <source>Percentile</source>
-        <translation>Perzentile</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -126,7 +126,7 @@
     <message>
         <location filename="../algs/scipy_statistical_algorithms.py" line="394"/>
         <source>Rank</source>
-        <translation>Rangordnung</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -134,42 +134,42 @@
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="605"/>
         <source>Size of flat structuring element (either size or footprint must be given, with footprint, size is ignored)</source>
-        <translation>Größe eines flachen Strukturelements (entweder Größe oder Fußabdruck müssen gegeben werden, mit Fußabdruck wird Größe ignoriert)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="620"/>
         <source>Size</source>
-        <translation>Größe</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="635"/>
         <source>Footprint array</source>
-        <translation>Fußabdruck</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="654"/>
         <source>Origin</source>
-        <translation>Ursprung</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="690"/>
         <source>Sizes does not match number of dimensions</source>
-        <translation>Größen-Liste passt nicht zur Anzahl der Dimensionen</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="697"/>
         <source>Origin does not match number of dimensions</source>
-        <translation>Ursprung-Liste muss zur Anzahl der Dimensionen passen</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="701"/>
         <source>Origin out of bounds of structure</source>
-        <translation>Ursprung befindet sich außerhalb der Struktur</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../scipy_algorithm_baseclasses.py" line="718"/>
         <source>Rank must be smaller than the size of the footprint</source>
-        <translation>Rangordnung muss kleiner als die Größe des Fußabdrucks sein</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -177,17 +177,17 @@
     <message>
         <location filename="../algs/scipy_statistical_algorithms.py" line="462"/>
         <source>Size of flat structuring element (either size or footprint must be given, with footprint, size is ignored)</source>
-        <translation>Größe eines flachen Strukturelements (entweder Größe oder Fußabdruck müssen angegeben werden, mit Fußabdruck wird Größe ignoriert)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../algs/scipy_statistical_algorithms.py" line="477"/>
         <source>Size</source>
-        <translation>Größe</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../algs/scipy_statistical_algorithms.py" line="501"/>
         <source>Sizes does not match number of dimensions</source>
-        <translation>Größe passt nicht zur Anzahl der Dimensionen</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
