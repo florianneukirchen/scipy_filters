a946fb27174bc36148911343b9c0b7b4909c2139
Florian Neukirchen <mail@riannek.de>
2024-03-24 11:01:06 +0100

    Start German translation

---
+++ i18n/scipy_filters_de.ts
@@ -0,0 +1,160 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="176"/>
+        <source>Input layer</source>
+        <translation>Eingabe-Layer</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="185"/>
+        <source>Dimension</source>
+        <translation>Dimensionen</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="205"/>
+        <source>Output data type</source>
+        <translation>Ausgabe-Datentyp</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="291"/>
+        <source>Failed to open Mask Layer</source>
+        <translation>Konnte Maskenlayer nicht öffnen</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="416"/>
+        <source>3D only possible if input layer has more than 1 bands</source>
+        <translation>3D ist nur bei Layern mit mehr als einem Band möglich</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="440"/>
+        <source>Warning, the range of output datatype is not in the range of the input datatype. Clipping is likely.</source>
+        <translation>Warnung, der Wertebereich des Ausgabedatentyps ist nicht innerhalb desjenigen des Eingabedatentyps. Die Werte werden vermutlich beschnitten.</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="520"/>
+        <source>Border Mode</source>
+        <translation>Randmodus</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="526"/>
+        <source>Constant value past edges for border mode &quot;constant&quot;</source>
+        <translation>Konstanter Wert für Randmodus &quot;constant&quot;</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="559"/>
+        <source>Axis</source>
+        <translation>Achse</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="589"/>
+        <source>Band axis not possible in 2D case</source>
+        <translation>Achse &quot;Band&quot; ist in 2D nicht möglich</translation>
+    </message>
+    <message>
+        <location filename="../algs/scipy_statistical_algorithms.py" line="462"/>
+        <source>Size of flat structuring element (either size or footprint must be given, with footprint, size is ignored)</source>
+        <translation>Größe eines flachen Strukturelements(entweder Größe oder Fußabdruck müssen gegeben sein, mit Fußabdruck wird Größe ignoriert)</translation>
+    </message>
+    <message>
+        <location filename="../algs/scipy_statistical_algorithms.py" line="477"/>
+        <source>Size</source>
+        <translation>Größe</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="637"/>
+        <source>Footprint array</source>
+        <translation>Fußabdruck</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="656"/>
+        <source>Origin</source>
+        <translation>Ursprung</translation>
+    </message>
+    <message>
+        <location filename="../algs/scipy_statistical_algorithms.py" line="501"/>
+        <source>Sizes does not match number of dimensions</source>
+        <translation>Größe passt nicht zur Anzahl der Dimensionen</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="699"/>
+        <source>Origin does not match number of dimensions</source>
+        <translation>Ursprung passt nicht zur Anzahl der Dimensionen</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="703"/>
+        <source>Origin out of bounds of structure</source>
+        <translation>Ursprung liegt außerhalb des Fußabdrucks</translation>
+    </message>
+    <message>
+        <location filename="../scipy_algorithm_baseclasses.py" line="720"/>
+        <source>Rank must be smaller than the size of the footprint</source>
+        <translation>Rangordnung muss kleiner sein als Größe des Fußabdrucks</translation>
+    </message>
+    <message>
+        <location filename="../algs/scipy_statistical_algorithms.py" line="305"/>
+        <source>Percentile</source>
+        <translation>Perzentile</translation>
+    </message>
+    <message>
+        <location filename="../algs/scipy_statistical_algorithms.py" line="394"/>
+        <source>Rank</source>
+        <translation>Rangordnung</translation>
+    </message>
+</context>
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="../ui/StructureWidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/SizesWidget.ui" line="20"/>
+        <source>Size (all axes)</source>
+        <translation>Größe (alle Achsen)</translation>
+    </message>
+    <message>
+        <location filename="../ui/SizesWidget.ui" line="37"/>
+        <source>Size (bands)</source>
+        <translation>Größe (Bänder)</translation>
+    </message>
+    <message>
+        <location filename="../ui/SizesWidget.ui" line="47"/>
+        <source>Size (rows)</source>
+        <translation>Größe (Zeilen)</translation>
+    </message>
+    <message>
+        <location filename="../ui/SizesWidget.ui" line="57"/>
+        <source>Size (cols)</source>
+        <translation>Größe (Spalten)</translation>
+    </message>
+    <message>
+        <location filename="../ui/OriginWidget.ui" line="23"/>
+        <source>Cols</source>
+        <translation>Spalten</translation>
+    </message>
+    <message>
+        <location filename="../ui/OriginWidget.ui" line="46"/>
+        <source>Rows</source>
+        <translation>Zeilen</translation>
+    </message>
+    <message>
+        <location filename="../ui/OriginWidget.ui" line="53"/>
+        <source>Bands</source>
+        <translation>Bänder</translation>
+    </message>
+    <message>
+        <location filename="../ui/StructureWidget.ui" line="29"/>
+        <source>Load</source>
+        <translation>Laden</translation>
+    </message>
+    <message>
+        <location filename="../ui/StructureWidget.ui" line="67"/>
+        <source>TextLabel</source>
+        <translation>TextLabel</translation>
+    </message>
+</context>
+</TS>
