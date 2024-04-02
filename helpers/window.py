a932cd1e7ffb6f64354a4233191725bd77187665
Florian Neukirchen <mail@riannek.de>
2024-04-02 10:17:54 +0200

    docstrings

--- helpers/window.py
+++ helpers/window.py
@@ -187,6 +187,11 @@ def wrap_margin(a, dataset, win: RasterWindow, band=None):
     This makes mode="wrap" (offered by some scipy filters) possible.
     The wrapping itself is done by scipy, this function only fills the
     data needed for wrapping to work.
+
+    a: Numpy array
+    dataset: gdal dataset
+    win: RasterWindow
+    band: int | None. Number of band (2D array), None for all bands (3D array).
     """
     rasterXSize = dataset.RasterXSize
     rasterYSize = dataset.RasterYSize
