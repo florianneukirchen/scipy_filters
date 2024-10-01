7d86de9010be9d350fe1a6e8b6b9a8b118627a6e
Florian Neukirchen <mail@riannek.de>
2024-10-01 15:46:03 +0200

    Add copyright and license note to unittests

--- test/test_pca.py
+++ test/test_pca.py
@@ -1,3 +1,16 @@
+"""PCA algorithms test.
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+
+__author__ = 'mail@riannek.de'
+__date__ = '2023-05-05'
+__copyright__ = 'Copyright 2023, Florian Neukirchen'
+
 import unittest
 import os
 import sys
