a16423cc13e79ee989c5b29e8eadaaf0604a8e55
Florian Neukirchen <mail@riannek.de>
2024-10-01 13:14:50 +0200

    Add unittest with shifted origin

--- test/test_convolution.py
+++ test/test_convolution.py
@@ -44,9 +44,13 @@ class TestConvolutionFilters(unittest.TestCase):
         output = processing.run("scipy_filters:fft_correlate", {'INPUT':testfile,'KERNEL':'[[1, 2, 1],\n[2, 4, 2],\n[1, 2, 1]]','NORMALIZATION':0,'BANDSTATS':True,'DTYPE':6,'OUTPUT':'TEMPORARY_OUTPUT'})
         self.assertEqual(rasterhash(output['OUTPUT']), '5574bd202e0d859150b78e20ead6d893deb39edf69c5905b22fe3c12', "CorrelateFFT hash does not match")
 
-    def test_convoluton_3D(self):
+    def test_convolution_3D(self):
         output = processing.run("scipy_filters:convolve", {'INPUT':testfile,'DIMENSION':1,'KERNEL':'[[[0.0, 1.0, 0.0],\n[1.0, 1.0, 1.0],\n[0.0, 1.0, 0.0]],\n[[1.0, 1.0, 1.0],\n[1.0, 1.0, 1.0],\n[1.0, 1.0, 1.0]],\n[[0.0, 1.0, 0.0],\n[1.0, 1.0, 1.0],\n[0.0, 1.0, 0.0]]]','NORMALIZATION':0,'ORIGIN':'0, 0, 0','BANDSTATS':True,'DTYPE':6,'OUTPUT':'TEMPORARY_OUTPUT','MODE':0,'CVAL':0})
         self.assertEqual(rasterhash(output['OUTPUT']), '6bed2d27fa14d4263f69f834656e7787ba9dc6034206f44c852a1241', "3D Convolution hash does not match")
+
+    def test_convolve_cross_shifted_origin(self):
+        output = processing.run("scipy_filters:convolve", {'INPUT':testfile,'DIMENSION':0,'KERNEL':'[[0.0, 1.0, 0.0],\n[1.0, 1.0, 1.0],\n[0.0, 1.0, 0.0]]','NORMALIZATION':0,'ORIGIN':'-1, 1','BANDSTATS':True,'DTYPE':6,'OUTPUT':'TEMPORARY_OUTPUT','MODE':0,'CVAL':0})
+        self.assertEqual(rasterhash(output['OUTPUT']), '5ac60dc5a402c916105cfa410ebf873e1e0c1619f0c9cf226f0bd39b', "Shifted origin hash does not match")
 
 
 if __name__ == '__main__':
