de44b2b15f115c6591da88b4703e629796e11f59
Florian Neukirchen <mail@riannek.de>
2024-10-01 09:10:57 +0200

    Docstring

--- helpers/rasterhash.py
+++ helpers/rasterhash.py
@@ -5,6 +5,18 @@ from qgis.utils import iface
 
 
 def rasterhash(rlayer=None):
+    """Return a SHA224 hash of the raster data for unittests
+
+    Can be used to compare raster data between runs of the same algorithm.
+    It is calucated in the same way as in QGIS core TestTools.py, 
+    but more convienient to use e.g. in the console. 
+    Default is the active layer.
+    
+    :param rlayer: raster layer or file path, default is active layer.
+    :type rlayer: QgsRasterLayer or str, optional
+    :return: SHA224 hash of the raster data
+    :rtype: str
+    """
     if rlayer is None:
         rlayer = iface.activeLayer()
     if isinstance(rlayer, QgsRasterLayer):
