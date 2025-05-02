327e6a31a2b8bb58beed28f5848ac6f42a3a2fec
Florian Neukirchen <mail@riannek.de>
2025-05-02 16:18:16 +0200

    Example in rasterhash docs

--- helpers/rasterhash.py
+++ helpers/rasterhash.py
@@ -18,6 +18,11 @@ def rasterhash(rlayer=None):
     :type rlayer: QgsRasterLayer or str, optional
     :return: SHA224 hash of the raster data
     :rtype: str
+
+    Example:: 
+        >>> from scipy_filters.helpers import rasterhash
+        >>> rasterhash()
+        'a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6a7b8c9d0e1f2'
     """
     if rlayer is None:
         rlayer = iface.activeLayer()
