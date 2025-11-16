9aa7a55bd1c5c699e2dff07d692769797e158c85
Florian Neukirchen <mail@riannek.de>
2025-11-16 10:46:36 +0100

    More parameter types

--- ui/scipy_processing_dialog.py
+++ ui/scipy_processing_dialog.py
@@ -1,9 +1,23 @@
-from qgis.gui import QgsProcessingAlgorithmDialogBase, QgsPanelWidget
+from qgis.gui import (
+    QgsProcessingAlgorithmDialogBase, 
+    QgsPanelWidget, 
+    QgsMapLayerComboBox,
+)
 from qgis.PyQt.QtWidgets import (
    QWidget, QVBoxLayout, QHBoxLayout, QLabel,
-   QSpinBox, QCheckBox, QLineEdit, QComboBox
+   QSpinBox, QCheckBox, QLineEdit, QComboBox,
+   QDoubleSpinBox
 )
-from qgis.core import QgsProcessingParameterDefinition# , QgsProcessingContext
+from qgis.core import (
+    QgsProcessingParameterDefinition,
+    QgsProcessingParameterRasterLayer,
+    QgsProcessingParameterRasterDestination,
+    QgsProcessingParameterEnum,
+    QgsProcessingParameterNumber,
+    QgsMapLayerProxyModel,
+    # , QgsProcessingContext
+)
+
 from qgis import processing 
 
 class ScipyProcessingDialog(QgsProcessingAlgorithmDialogBase):
@@ -30,8 +44,6 @@ class ScipyProcessingDialog(QgsProcessingAlgorithmDialogBase):
 
     def buildUI(self):
         
-
-
         for param in self._alg.parameterDefinitions():
             label, widget = self.createWidgetForParameter(param)
 
@@ -48,12 +60,56 @@ class ScipyProcessingDialog(QgsProcessingAlgorithmDialogBase):
         self.setMainWidget(self.panel)
 
     def createWidgetForParameter(self, param: QgsProcessingParameterDefinition):
-        """Factory to create widgets based on parameter type."""
-
-        ptype = param.type()
+        """Return (label, widget) for any supported parameter."""
         label = param.description()
 
-        # Integer
+        if isinstance(param, QgsProcessingParameterRasterLayer):
+            w = QgsMapLayerComboBox()
+            w.setFilters(QgsMapLayerProxyModel.RasterLayer)
+            if param.defaultValue():
+                w.setLayer(param.defaultValue())
+            return label, w
+
+        if isinstance(param, QgsProcessingParameterRasterDestination):
+            # Output destinations are simple line edits
+            w = QLineEdit()
+            if param.defaultValue():
+                w.setText(param.defaultValue())
+            return label, w
+
+        if isinstance(param, QgsProcessingParameterEnum):
+            w = QComboBox()
+            for opt in param.options():
+                w.addItem(opt)
+            if param.defaultValue() is not None:
+                w.setCurrentIndex(param.defaultValue())
+            return label, w
+
+        if isinstance(param, QgsProcessingParameterNumber):
+            if param.dataType() == QgsProcessingParameterNumber.Integer:
+
+                w = QSpinBox()
+                if param.minimum() is not None:
+                    w.setMinimum(int(param.minimum()))
+                if param.maximum() is not None:
+                    w.setMaximum(int(param.maximum()))
+                if param.defaultValue() is not None:
+                    w.setValue(int(param.defaultValue()))
+                return label, w
+            else:
+                # Float
+                w = QDoubleSpinBox()
+                if param.minimum() is not None:
+                    w.setMinimum(param.minimum())
+                if param.maximum() is not None:
+                    w.setMaximum(param.maximum())
+                if param.defaultValue() is not None:
+                    w.setValue(float(param.defaultValue()))
+                return label, w
+
+        ptype = param.type()
+
+        # Integer fallback
         if ptype == "int":
             spin = QSpinBox()
             spin.setValue(param.defaultValue() or 0)
@@ -73,8 +129,40 @@ class ScipyProcessingDialog(QgsProcessingAlgorithmDialogBase):
                 edit.setText(str(param.defaultValue()))
             return label, edit
 
-        # Let QGIS insert its own standard widget: return None
+        print(label, "-", ptype, param)
         return None, None
+
+
+
+    # def createWidgetForParameter(self, param: QgsProcessingParameterDefinition):
+    #     """Factory to create widgets based on parameter type."""
+
+    #     ptype = param.type()
+    #     label = param.description()
+
+    #     # Integer
+    #     if ptype == "int":
+    #         spin = QSpinBox()
+    #         spin.setValue(param.defaultValue() or 0)
+    #         spin.setRange(-999999999, 999999999)
+    #         return label, spin
+
+    #     # Boolean
+    #     if ptype == "boolean":
+    #         chk = QCheckBox(label)
+    #         chk.setChecked(bool(param.defaultValue()))
+    #         return None, chk
+
+    #     # String
+    #     if ptype == "string":
+    #         edit = QLineEdit()
+    #         if param.defaultValue():
+    #             edit.setText(str(param.defaultValue()))
+    #         return label, edit
+
+    #     # Let QGIS insert its own standard widget: return None
+    #     print(label, "-", ptype, param)
+    #     return None, None
 
 
 
